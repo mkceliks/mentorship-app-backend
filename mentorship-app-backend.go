f1d43a4ac7f1572b65c3675948568295c8eb1f2c
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-10-20 14:21:56 +0400

    deployment flow node upd

--- mentorship-app-backend.go
+++ mentorship-app-backend.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecr"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
@@ -23,29 +24,44 @@ func NewMentorshipAppBackendStack(scope constructs.Construct, id string, props *
 		bucketName = "MentorshipAppBucket-Production"
 	}
 
+	// Create the S3 bucket
 	bucket := awss3.NewBucket(stack, jsii.String(bucketName), &awss3.BucketProps{
 		Versioned: jsii.Bool(true),
 	})
 
+	// Reference the ECR repository
+	ecrRepository := awsecr.Repository_FromRepositoryName(stack, jsii.String("MentorshipAppRepository"), jsii.String("mentorship-app"))
+
+	// Create the Lambda function using the ECR image
 	uploadLambda := awslambda.NewFunction(stack, jsii.String("UploadLambda"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2(),
+		Code:    awslambda.Code_FromEcrImage(ecrRepository, nil), // Load the image from ECR
 		Handler: jsii.String("bootstrap"),
-		Code:    awslambda.Code_FromAsset(jsii.String("./function.zip"), nil),
 		Environment: &map[string]*string{
-			"BUCKET_NAME": bucket.BucketName(),
+			"BUCKET_NAME": jsii.String(bucketName),
 		},
 	})
 
-	bucket.GrantReadWrite(uploadLambda, jsii.String("*"))
+	// Grant the Lambda permissions to read/write to the S3 bucket
+	bucket.GrantReadWrite(uploadLambda, "*") // Grants read/write to all objects in the bucket
 
-	bucket.AddToResourcePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+	// Add S3 specific permissions to the Lambda's execution role (explicitly listing permissions)
+	uploadLambda.AddToRolePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
 		Actions:   jsii.Strings("s3:PutObject", "s3:GetObject"),
 		Resources: jsii.Strings(fmt.Sprintf("%s/*", *bucket.BucketArn())),
-		Principals: &[]awsiam.IPrincipal{
-			awsiam.NewArnPrincipal(uploadLambda.Role().RoleArn()),
-		},
 	}))
 
+	// Add ECR permissions to the Lambda's execution role for pulling the Docker image
+	uploadLambda.AddToRolePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Actions: jsii.Strings(
+			"ecr:GetDownloadUrlForLayer",
+			"ecr:BatchGetImage",
+			"ecr:DescribeImages",
+		),
+		Resources: jsii.Strings(*ecrRepository.RepositoryArn()),
+	}))
+
+	// Set up API Gateway and integrate it with Lambda
 	api := awsapigateway.NewRestApi(stack, jsii.String("MentorshipAppAPI"), &awsapigateway.RestApiProps{
 		RestApiName: jsii.String("MentorshipAppAPI"),
 		Description: jsii.String("API Gateway for handling S3 file uploads."),
