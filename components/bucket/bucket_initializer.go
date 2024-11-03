8f89afb5bc54627c6a6415dbe75b044ce93231a2
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-03 19:40:53 +0400

    bucket name fix

--- components/bucket/bucket_initializer.go
+++ components/bucket/bucket_initializer.go
@@ -1,20 +1,15 @@
 package bucket
 
 import (
-	"fmt"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	"github.com/aws/jsii-runtime-go"
-	"log"
 )
 
 func InitializeBucket(stack awscdk.Stack, bucketName string) awss3.Bucket {
-	finalBucketName := fmt.Sprintf("%s", bucketName)
 
-	log.Printf("Creating bucket with name: %s", finalBucketName)
-
-	return awss3.NewBucket(stack, jsii.String(finalBucketName), &awss3.BucketProps{
-		BucketName: jsii.String(finalBucketName),
+	return awss3.NewBucket(stack, jsii.String(bucketName), &awss3.BucketProps{
+		BucketName: jsii.String(bucketName),
 		Versioned:  jsii.Bool(true),
 	})
 }
