82168b0750de507fa8b7cb651d6d5879f0339195
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-08 00:19:38 +0400

    secret parsing json

--- components/secrets/get_secret.go
+++ components/secrets/get_secret.go
@@ -2,27 +2,38 @@ package secrets
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
-func GetSecretValue(secretArn string) (string, error) {
+func GetSecretValue(secretARN string) (string, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		return "", fmt.Errorf("unable to load SDK config, %v", err)
 	}
 
-	svc := secretsmanager.NewFromConfig(cfg)
+	client := secretsmanager.NewFromConfig(cfg)
 
 	input := &secretsmanager.GetSecretValueInput{
-		SecretId: aws.String(secretArn),
+		SecretId: aws.String(secretARN),
 	}
-	result, err := svc.GetSecretValue(context.TODO(), input)
+	result, err := client.GetSecretValue(context.TODO(), input)
 	if err != nil {
 		return "", fmt.Errorf("failed to retrieve secret: %v", err)
 	}
 
-	return *result.SecretString, nil
+	var secretData map[string]string
+	if err = json.Unmarshal([]byte(*result.SecretString), &secretData); err != nil {
+		return "", fmt.Errorf("failed to parse secret JSON: %v", err)
+	}
+
+	slackWebhookURL, ok := secretData["slack_webhook_url"]
+	if !ok {
+		return "", fmt.Errorf("slack_webhook_url key not found in secret")
+	}
+
+	return slackWebhookURL, nil
 }
