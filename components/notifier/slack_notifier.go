d558150de2fc9fbb5730b753cdbf8e3b147cc7b5
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-08 02:29:18 +0400

    channel join process

--- components/notifier/slack_notifier.go
+++ components/notifier/slack_notifier.go
@@ -25,14 +25,19 @@ func NotifySlack(token, baseChannel, message string, fields []slack.AttachmentFi
 
 	api := slack.New(token)
 
+	_, _, _, err := api.JoinConversation(channel)
+	if err != nil {
+		log.Printf("Unable to join conversation %s: %v", channel, err)
+	}
+
 	attachment := slack.Attachment{
 		Color:  color,
 		Text:   message,
 		Fields: fields,
 	}
 
-	_, _, err := api.PostMessage(channel, slack.MsgOptionAttachments(attachment))
+	_, _, postErr := api.PostMessage(channel, slack.MsgOptionAttachments(attachment))
 	if err != nil {
-		log.Printf("Failed to send message to Slack channel %s: %v", channel, err)
+		log.Printf("Failed to send message to Slack channel %s: %v", channel, postErr)
 	}
 }
